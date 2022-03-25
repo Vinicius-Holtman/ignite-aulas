197eaca7db4ce1f5bbcd4d8c454d64907fb140c6
vinicius <viniholtman28@gmail.com>
2022-03-25 00:18:39 -0300

    Migrado projeto para typescript

---
+++ reactjs/01-github-explorer/src/components/RepositoryItem.tsx
@@ -0,0 +1,22 @@
+
+
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+}
+
+export function RepositoryItem({ repository }: RepositoryItemProps) {
+  return (
+    <li>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
+
+      <a href={repository.html_url}>
+        Link Repositorio
+      </a>
+    </li>
+  )
+}
