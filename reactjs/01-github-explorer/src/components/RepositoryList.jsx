6169dce058f55ad06a0b33acc6fc66cc75f9f4aa
vinicius <viniholtman28@gmail.com>
2022-03-24 23:18:17 -0300

    Renderizado data repo no front

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -3,14 +3,6 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
 
-// https://api.github.com/orgs/rocketseat/repos
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -25,10 +17,10 @@ export function RepositoryList() {
       <h1>Lista de repositorios</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repo => {
+          return <RepositoryItem key={repo.name} repository={repo}/>
+        })}
+        
       </ul>
     </section>
   )
